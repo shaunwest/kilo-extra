d92e51f7001b5ed23401afe6b0bc80653bdabfad
Shaun West <shaunwest@gmail.com>
2015-01-18 20:00:46 -0500

    Http detects CommonJS

--- export.js
+++ export.js
@@ -1,8 +1,9 @@
-var kilo, use, register;
+var kilo, use, register, CommonJS = false;
 if(typeof exports === 'object' && typeof require === 'function') {
   kilo = require('kilo');
   if(kilo) {
     use = kilo.use;
     register = kilo.register;
+    CommonJS = true;
   }
 }
--- src/http.js
+++ src/http.js
@@ -2,62 +2,130 @@
  * Created by Shaun on 5/3/14.
  */
 
-register('Http', ['Util'], function(Util) {
-  'use strict';
-
-  function parseResponse(contentType, responseText) {
-    switch(contentType) {
-      case 'application/json':
-      case 'application/json; charset=utf-8':
-        return JSON.parse(responseText);
-      default:
-        return responseText;
+if(CommonJS) {
+  kilo.register('Http', ['Util'], function(Util) {
+    function parseResponse(contentType, responseText) {
+      switch(contentType) {
+        case 'application/json':
+        case 'application/json; charset=utf-8':
+          return JSON.parse(responseText);
+        default:
+          return responseText;
+      }
     }
-  }
 
-  function get(url, contentTypeOrOnProgress, onProgress) {
-    return new Promise(function(resolve, reject) {
-      var req = new XMLHttpRequest();
+    function get(url, contentTypeOrOnProgress, onProgress) {
+      var promise = new Promise(function(resolve, reject) {
+        var req = http.request(url, function(res) {
+          var data = '';
 
-      if(Util.isFunction(contentTypeOrOnProgress)) {
-        onProgress = contentTypeOrOnProgress;
-        contentTypeOrOnProgress = null;
+          if(Util.isFunction(contentTypeOrOnProgress)) {
+            onProgress = contentTypeOrOnProgress;
+            contentTypeOrOnProgress = null;
+          }
+
+          res.setEncoding('utf8');
+
+          res.on('data', function (chunk) {
+            data += chunk;
+            if(onProgress) {
+              onProgress(chunk.length, data.length);
+            }
+          });
+
+          res.on('end', function() {
+            var contentType = res.headers['content-type'];
+            switch(res.statusCode) {
+              case 500:
+                reject({statusText: '', status: res.statusCode});
+                break;
+              case 404:
+                reject({statusText: '', status: res.statusCode});
+                break;
+              case 304:
+                resolve({data: parseResponse(contentType, data), status: res.statusCode});
+                break;
+              default:
+                resolve({data: parseResponse(contentType, data), status: res.statusCode});
+            }
+          });
+        });
+
+        req.on('error', function(e) {
+          console.log('problem with request: ' + e.message);
+          console.log('URL: ' + url);
+          reject('Network error.');
+        });
+
+        req.end();
+      });
+
+      return promise;
+    }
+
+    return {
+      get: get
+    };
+  });
+} else {
+  register('Http', ['Util'], function(Util) {
+    'use strict';
+
+    function parseResponse(contentType, responseText) {
+      switch(contentType) {
+        case 'application/json':
+        case 'application/json; charset=utf-8':
+          return JSON.parse(responseText);
+        default:
+          return responseText;
       }
+    }
 
-      if(onProgress) {
-        req.addEventListener('progress', function(event) {
-          onProgress(event.loaded, event.total);
-        }, false);
-      }
+    function get(url, contentTypeOrOnProgress, onProgress) {
+      return new Promise(function(resolve, reject) {
+        var req = new XMLHttpRequest();
 
-      req.onerror = function(event) {
-        reject(Util.error('Network error.'));
-      };
-
-      req.onload = function() {
-        var contentType = contentTypeOrOnProgress || this.getResponseHeader('content-type');
-
-        switch(this.status) {
-          case 500:
-            reject({statusText: this.statusText, status: this.status});
-            break;
-          case 404:
-            reject({statusText: this.statusText, status: this.status});
-            break;
-          case 304:
-            resolve({data: parseResponse(contentType, this.responseText), status: this.status});
-            break;
-          default:
-            resolve({data: parseResponse(contentType, this.responseText), status: this.status});
+        if(Util.isFunction(contentTypeOrOnProgress)) {
+          onProgress = contentTypeOrOnProgress;
+          contentTypeOrOnProgress = null;
         }
-      };
 
-      req.open('get', url, true);
-      req.send();
-    });
-  }
+        if(onProgress) {
+          req.addEventListener('progress', function(event) {
+            onProgress(event.loaded, event.total);
+          }, false);
+        }
 
-  return {
-    get: get
-  };
-});
+        req.onerror = function(event) {
+          reject(Util.error('Network error.'));
+        };
+
+        req.onload = function() {
+          var contentType = contentTypeOrOnProgress || this.getResponseHeader('content-type');
+
+          switch(this.status) {
+            case 500:
+              reject({statusText: this.statusText, status: this.status});
+              break;
+            case 404:
+              reject({statusText: this.statusText, status: this.status});
+              break;
+            case 304:
+              resolve({data: parseResponse(contentType, this.responseText), status: this.status});
+              break;
+            default:
+              resolve({data: parseResponse(contentType, this.responseText), status: this.status});
+          }
+        };
+
+        req.open('get', url, true);
+        req.send();
+      });
+    }
+
+    return {
+      get: get
+    };
+  });
+}
+
